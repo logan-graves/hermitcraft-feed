dec50325d1a18d2d4d759d9ab355cf37bbf2689f
Logan Graves <github@logangraves.com>
2021-06-16 23:44:16 -0700

    Second restyle, create "popular" section

--- script.js
+++ script.js
@@ -15,21 +15,51 @@ window.onload = function(){
 }
 
 function useJSON(data) {
-    console.log(data)
+    console.log(data);
+    var x = 0;
+    for (i of data) {
+        x = x + 1;
+        var workingVideo = data[x];
+        if (x == 7) {
+            break;
+        } else {
+            var popularDiv = document.getElementById("popular");
+            
+            // Create Greater Div (Div that holds iframe and title)
+            var greaterDiv = document.createElement("div");
+            greaterDiv.setAttribute("class", "greaterDiv");
+            popularDiv.appendChild(greaterDiv);
+            
+            //Create video div (div that holds the iframe)
+            var videoDiv = document.createElement("div");
+            videoDiv.setAttribute("class", "videoDiv");
+            greaterDiv.appendChild(videoDiv)
+            
+            //Create iframe
+            var iframe = document.createElement("iframe")
+            iframe.setAttribute("src", "https://invidious.fdn.fr/embed/" + workingVideo.videoId);
+            videoDiv.appendChild(iframe);
+
+            //Create Link for video 
+            var videoLink = document.createElement("a")
+            videoLink.setAttribute("href", "https://www.yewtu.be/watch?v=" + workingVideo.videoId)
+            greaterDiv.appendChild(videoLink)
+            
+            //Create video Title element
+            var videoTitle = document.createElement("p");
+            videoTitle.setAttribute("class", "videoTitle");
+            videoTitle.textContent = workingVideo.title;
+            //put the video title inside of the A element
+            videoLink.appendChild(videoTitle);
+
+            //Create author Element
+            var author = document.createElement("p");
+            author.setAttribute("class", "author");
+            author.textContent = workingVideo.author;
+            greaterDiv.appendChild(author);
+        }
+    }
 }
-
-
-
-
-
-// don't forget to un comment the script.js link
-
-
-
-//var videoId = data.videoId;
-//var videoElement = document.getElementById("test");
-//videoElement.src = "https://invidious.ggc-project.de/embed/" + videoId;
-
 
 /* BDoubleO cubfan135 Docm77 Etho False Grian Hypno impulseSV iskall85 Jevin joehills Keralis MumboJumbo 
 rendog Scar Stress Tango Tek TinfoilChef Vintage Beef Welsknight xBCrafted Xisuma ZedaphPlays ZombieCleo */
